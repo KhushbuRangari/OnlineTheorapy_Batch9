37982773a556a07d889a4c3e3ce71036585cc89d
Reeth <rayith.bose18@gmail.com>
2023-10-15 11:56:53 +0530

    Updated Doctor Page

--- onlineclinic/src/App.js
+++ onlineclinic/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/pages/Banner';
 import Services from './components/pages/Services';
+import SecurityInfo from './components/pages/SecurityInfo';
 import Info from './components/pages/Info';
 function App() {
   return (
     <>
     <Banner />
     <Services />
-
+    <SecurityInfo />
     <Info />
     </>
   );
---
+++ onlineclinic/src/components/pages/SecurityInfo.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function SecurityInfo() {
+  return (
+    <div className="security">
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-12">
+            <h3>Safety of your data is our top priority.</h3>
+            <ul>
+              <li>Multi-level security checks</li>
+              <li>Multiple data backups</li>
+              <li>Stringent data privacy policies</li>
+            </ul>
+            <button className="btn btn-primary">Read more</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
